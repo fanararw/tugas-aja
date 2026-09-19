e7c437fa134bb5b7b827be95e6460d16725529c6
fanararw <fanararw@gmail.com>
2026-09-19 11:21:49 +0800

    Add files via upload

---
+++ FanaRarw.java
@@ -0,0 +1,7 @@
+public class FanaRarw {
+    public static void main(String[] args) {
+        System.out.println("Fana...");
+        System.out.println("Sometime feel like i'm not enough?");
+        System.out.println("But i'm trying my best");
+    }
+}
